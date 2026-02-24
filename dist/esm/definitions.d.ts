55f53336df23b8061b96b92e2eea2969853457aa
Alessandro Spagnolo <alessandrospagnolo6@gmail.com>
2026-02-24 17:20:06 +0100

    build(dialog): dist for v8.0.1.6

---
+++ dist/esm/definitions.d.ts
@@ -0,0 +1,179 @@
+export interface AlertOptions {
+    /**
+     * Title of the dialog.
+     *
+     * @since 1.0.0
+     */
+    title?: string;
+    /**
+     * Message to show on the dialog.
+     *
+     * @since 1.0.0
+     */
+    message: string;
+    /**
+     * Text to use on the action button.
+     *
+     * @default "OK"
+     * @since 1.0.0
+     */
+    buttonTitle?: string;
+    /**
+     * Style to use on action button (IOS Style)
+     *
+     * @default "default"
+     */
+    buttonStyle?: 'default' | 'destructive' | 'cancel';
+    /**
+     * ANDROID ONLY: Whether the alert uses the DeviceTheme or the default theme used by Capacitor (AppCompat)
+     *
+     * @default "false"
+     */
+    useDefaultDialogStyle?: boolean;
+}
+export interface PromptOptions {
+    /**
+     * Title of the dialog.
+     *
+     * @since 1.0.0
+     */
+    title?: string;
+    /**
+     * Message to show on the dialog.
+     *
+     * @since 1.0.0
+     */
+    message: string;
+    /**
+     * Text to use on the positive action button.
+     *
+     * @default "OK"
+     * @since 1.0.0
+     */
+    okButtonTitle?: string;
+    /**
+     * Text to use on the negative action button.
+     *
+     * @default "Cancel"
+     * @since 1.0.0
+     */
+    cancelButtonTitle?: string;
+    /**
+     * Style to use on ok button (IOS-like)
+     *
+     * @default "default"
+     */
+    okButtonStyle?: 'default' | 'destructive' | 'cancel';
+    /**
+     * Style to use on cancel button (IOS-like)
+     *
+     * @default "default"
+     */
+    cancelButtonStyle?: 'default' | 'destructive' | 'cancel';
+    /**
+     * Placeholder text for hints.
+     *
+     * @since 1.0.0
+     */
+    inputPlaceholder?: string;
+    /**
+     * Prepopulated text.
+     *
+     * @since 1.0.0
+     */
+    inputText?: string;
+    /**
+     * ANDROID ONLY: Whether the alert uses the DeviceTheme or the default theme used by Capacitor (AppCompat)
+     *
+     * @default "false"
+     */
+    useDefaultDialogStyle?: boolean;
+}
+export interface ConfirmOptions {
+    /**
+     * Title of the dialog.
+     *
+     * @since 1.0.0
+     */
+    title?: string;
+    /**
+     * Message to show on the dialog.
+     *
+     * @since 1.0.0
+     */
+    message: string;
+    /**
+     * Text to use on the positive action button.
+     *
+     * @default "OK"
+     * @since 1.0.0
+     */
+    okButtonTitle?: string;
+    /**
+     * Text to use on the negative action button.
+     *
+     * @default "Cancel"
+     * @since 1.0.0
+     */
+    cancelButtonTitle?: string;
+    /**
+     * Style to use on ok button (IOS-like)
+     *
+     * @default "default"
+     */
+    okButtonStyle?: 'default' | 'destructive' | 'cancel';
+    /**
+     * Style to use on cancel button (IOS-like)
+     *
+     * @default "default"
+     */
+    cancelButtonStyle?: 'default' | 'destructive' | 'cancel';
+    /**
+     * ANDROID ONLY: Whether the alert uses the DeviceTheme or the default theme used by Capacitor (AppCompat)
+     *
+     * @default "false"
+     */
+    useDefaultDialogStyle?: boolean;
+}
+export interface PromptResult {
+    /**
+     * Text entered on the prompt.
+     *
+     * @since 1.0.0
+     */
+    value: string;
+    /**
+     * Whether if the prompt was canceled or accepted.
+     *
+     * @since 1.0.0
+     */
+    cancelled: boolean;
+}
+export interface ConfirmResult {
+    /**
+     * true if the positive button was clicked, false otherwise.
+     *
+     * @since 1.0.0
+     */
+    value: boolean;
+}
+export interface DialogPlugin {
+    /**
+     * Show an alert dialog
+     *
+     * @since 1.0.0
+     */
+    alert(options: AlertOptions): Promise<void>;
+    /**
+     * Show a prompt dialog
+     *
+     * @since 1.0.0
+     */
+    prompt(options: PromptOptions): Promise<PromptResult>;
+    /**
+     * Show a confirmation dialog
+     *
+     * @since 1.0.0
+     */
+    confirm(options: ConfirmOptions): Promise<ConfirmResult>;
+}
